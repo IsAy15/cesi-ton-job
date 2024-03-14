fbe8ffdfb7ed9649bccd7a7be7e57b7452f698d4
IsAy15 <40857644+IsAy15@users.noreply.github.com>
2024-03-14 12:00:44 +0100

    Ajout des fichiers de remplissage des tables applications, grades, offer_requirements et student_abilities

---
+++ database/fills/applications.sql
@@ -0,0 +1,10 @@
+INSERT INTO `applications` (`offer_id`,`user_id`,`cv`,`letter`) VALUES
+(1, 2, 'cv.pdf', 'lettre.pdf'),
+(1, 3, 'cv.pdf', 'lettre.pdf'),
+(1, 4, 'cv.pdf', 'lettre.pdf'),
+(2, 2, 'cv.pdf', 'lettre.pdf)',
+(2, 3, 'cv.pdf', 'lettre.pdf'),
+(2, 4, 'cv.pdf', 'lettre.pdf'),
+(3, 2, 'cv.pdf', 'lettre.pdf'),
+(3, 3, 'cv.pdf', 'lettre.pdf'),
+(3, 4, 'cv.pdf', 'lettre.pdf');
