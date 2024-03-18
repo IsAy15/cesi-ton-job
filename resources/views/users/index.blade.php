dc9828a838bf300e200a3d69f2e224368848f274
Achraf SOBHI <achrafsobhi.123@gmail.com>
2024-03-18 14:39:07 +0100

    Aucune idée de ce que j'ai push à part la barre de recherche pour les offres et les users

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -16,10 +16,13 @@
         <a href="{{ route('auth.login') }}">Se connecter</a> 
     @endguest
 
+    <!-- Barre de recherche -->
+    <input type="text" id="searchInput" placeholder="Rechercher un utilisateur...">
+    
     <!-- Bouton "Ajouter un utilisateur" -->
     <a href="{{ route('users.create') }}">Ajouter un utilisateur</a>
 
-    <table>
+    <table id="userTable">
         <thead>
             <tr>
                 <th>Nom</th>
@@ -49,5 +52,31 @@
             @endforeach
         </tbody>
     </table>
+
+    <script>
+        function filterUsers() {
+            var input, filter, table, tr, td, i, txtValue;
+            input = document.getElementById("searchInput");
+            filter = input.value.toUpperCase();
+            table = document.getElementById("userTable");
+            tr = table.getElementsByTagName("tr");
+            for (i = 0; i < tr.length; i++) {
+                td1 = tr[i].getElementsByTagName("td")[0];
+                td2 = tr[i].getElementsByTagName("td")[1];
+                if (td1 || td2) {
+                    txtValue1 = td1.textContent || td1.innerText;
+                    txtValue2 = td2.textContent || td2.innerText;
+                    if (txtValue1.toUpperCase().indexOf(filter) > -1 || txtValue2.toUpperCase().indexOf(filter) > -1) {
+                        tr[i].style.display = "";
+                    } else {
+                        tr[i].style.display = "none";
+                    }
+                }       
+            }
+        }
+
+        // Appeler la fonction de filtrage lorsque l'utilisateur tape quelque chose dans la barre de recherche
+        document.getElementById("searchInput").addEventListener("keyup", filterUsers);
+    </script>
 </body>
 </html>
