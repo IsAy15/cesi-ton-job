56b2f8f0be5db4e237d18bfcfca2f64011bf6e4d
SIADOUXValentin <valentin.siadoux@viacesi.fr>
2024-03-19 15:21:09 +0100

    footer et page connexion

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,22 +5,23 @@
         <h1>Connexion</h1>
         <form action="{{ route('auth.login') }}" method="post">
             @csrf
-            <div>
-                <label for="email">Email</label>
-                <input type="email" name="email" id="email">
+                <input type="email" name="email" id="email" placeholder="Email">
                 @error('email')
                     <div class="text-danger">{{ $message }}</div>
                 @enderror
-            </div>
-            <div class="form-group">
-                <label for="password">Mot de passe</label>
-                <input type="password" name="password" id="password" class="form-control">
+                <input type="password" name="password" id="password" placeholder="Mot de passe">
                 @error('password')
                     <div class="text-danger">{{ $message }}</div>
                 @enderror
-            </div>
-            <button type="submit" class="btn btn-primary">Se connecter</button>
+                <div class="mdp">
+                    <a href="#" onclick="alert('Alors connard ?\nTu as oublié ton mot de passe ?');">Mot de passe oublié ?</a>
+                </div>
+            <button type="submit">Se connecter</button>
         </form>
     </div>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <div class="container connexion">
+        <a href="#">Pas encore inscrit ?</a>
+        <a href="#" onclick="alert('Alors connard ?\nTu es nul ?');">Je n'arrive pas à me connecter</a>
+    </div>
+    <!-- <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script> -->
 @endsection
