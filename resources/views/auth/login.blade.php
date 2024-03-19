1765e4e4a11414008c7c43ccfc4fec1ec136fc2f
IsAy15 <40857644+IsAy15@users.noreply.github.com>
2024-03-19 16:40:18 +0100

    Fusion des push d'Adrien et Valentin sur le login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,14 +12,18 @@
         <h1>Connexion</h1>
         <form action="{{ route('auth.login') }}" method="post">
             @csrf
+            <div class="input-required">
                 <input type="email" name="email" id="email" placeholder="Email">
                 @error('email')
                     <div class="text-danger">{{ $message }}</div>
                 @enderror
+            </div>
+            <div class="input-required">
                 <input type="password" name="password" id="password" placeholder="Mot de passe">
                 @error('password')
                     <div class="text-danger">{{ $message }}</div>
                 @enderror
+            </div>
                 <div class="mdp">
                     <a href="#" onclick="alert('Alors connard ?\nTu as oublié ton mot de passe ?');">Mot de passe oublié ?</a>
                 </div>
