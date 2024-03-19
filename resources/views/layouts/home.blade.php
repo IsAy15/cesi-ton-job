3caf6103da3b796058a611c8de36ebd3d4716dca
IsAy15 <40857644+IsAy15@users.noreply.github.com>
2024-03-19 11:34:27 +0100

    Mise à jour des footers

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -18,16 +18,7 @@
     <main>
         @yield('content')
     </main>
-
-    <footer>
-        <div>
-
-            <p>CESI Ton Job &copy; <script>document.write(new Date().getFullYear());
-            </script></p>
-        </div>
-        <div>
-            <a href="{{ route('policy')}}">Politique de confidentialité</a>
-        </div>
-    </footer>
+    @extends('layouts.footer')
+    @section('footer')
 </body>
 </html>
