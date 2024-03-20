74183945fe36f019b75e5ac2a2790fe8db1c6aac
SIADOUXValentin <valentin.siadoux@viacesi.fr>
2024-03-20 15:29:32 +0100

    css login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.home')
 @section('title', 'Se connecter')
 @section('content')
+@vite('resources/css/login.css')
 <?php
     if (Auth::check()) {
         header('Location: ' . route('profile.index')); 
@@ -8,7 +9,7 @@
     }
 ?>
 
-    <div class="container connexion">
+    <div class="c-1 bg-1 connexion">
         <h1>Connexion</h1>
         <form action="{{ route('auth.login') }}" method="post">
             @csrf
@@ -27,12 +28,12 @@
                 <div class="mdp">
                     <a href="#" onclick="alert('Alors connard ?\nTu as oublié ton mot de passe ?');">Mot de passe oublié ?</a>
                 </div>
-            <button type="submit" class="btn">Se connecter</button>
+            <button type="submit" class="btn-1">Se connecter</button>
         </form>
     </div>
-    <div class="container connexion">
-        <a href="{{ route('auth.register') }}" class="btn">Pas encore inscrit ?</a>
-        <a href="#" class="btn"onclick="alert('Alors connard ?\nTu es nul ?');">Je n'arrive pas à me connecter</a>
+    <div class="c-1 bg-1 connexion">
+        <a href="{{ route('auth.register') }}" class="btn-1">Pas encore inscrit ?</a>
+        <a href="#" class="btn-1"onclick="alert('Alors connard ?\nTu es nul ?');">Je n'arrive pas à me connecter</a>
     </div>
     <!-- <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script> -->
 @endsection
