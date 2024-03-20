6952c58a033f43ef2489c1544984269dc1958b2b
SIADOUXValentin <valentin.siadoux@viacesi.fr>
2024-03-20 11:36:10 +0100

    suite page profil

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -4,52 +4,51 @@
 @vite('resources/css/profile.css')
     <div class="c-1">
         <div id="infos_perso" class="bandeau-1">
+            <div id="photo" class="c-2">
+                <img src="cesi-ton-job\public\images\icons\33645487-icC3B4ne-de-profil-avatar-portrait-masculin-personne-dC3A9contractC3A9e.png" />
+            </div>
             <div id="infos" class="infos">
-                <div id="nom"class="c-1 c-2">
-                    <p>{{ $user->lastname }}</p>
-                </div>
-                <div id="prenom"class="c-1 c-2">
-                    <p>{{ $user->firstname }}</p>
-                </div>
-                <div id="email" class="c-1 c-2">
-                    <p>{{ $user->email }}</p>
-                </div>
-                <div id="promo" class="c-1 c-2">
-                    <p>{{ $user->role }}</p>
-                </div>
+                    <p>{{ Auth::user()->lastname }}</p>
+                    <p>{{ Auth::user()->firstname }}</p>
+                    <p>{{ Auth::user()->email }}</p>
+                @if(Auth::user()->role=='user')
+                    @foreach(Auth::user()->promotions as $promotion)
+                        <p>{{ $promotion->name }}</p>
+                    @endforeach
+                @endif
             </div>
         </div>
-        <div>
-            <a href="{{ route('profile.offers') }}" class="btn btn-primary">Voir les offres postulées</a>
-        </div>
-        <div>
-            <a href="{{ route('profile.wishlist') }}" class="btn btn-primary">View Wishlist</a>
-        </div>
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Votre profil</div>
-                        <div class="card-body">
-                            @if($user->promotions->isNotEmpty())
-                                <div>
-                                    <h4>Promotion:</h4>
-                                    <ul>
-                                        @foreach($user->promotions as $promotion)
-                                            <li>{{ $promotion->name }}</li>
-                                        @endforeach
-                                    </ul>
-                                </div>
-                            @endif
-                            <div>
-                                <a href="{{ route('profile.offers') }}" class="btn btn-primary">Voir les offres postulées</a>
-                            </div>
-                            <div>
-                                <a href="{{ route('profile.wishlist') }}" class="btn btn-primary">View Wishlist</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+        @if(Auth::user()->role="user")
+            <div id="admin" class="c-3">
+                <p>Compétences :</p>
+                <div 
+                <p>Compétence 1</p>
+                <p>Compétence 2</p>
+                <p>Compétence 3</p>
+                <p>Compétence 4</p>
+                <p>Compétence 5</p>
             </div>
-        </div>
+        @endif
+        @if(Auth::user()->role=="admin")
+            <div class="interaction">
+                <a href="#" class="btn btn-primary">Voir les utilisateurs</a>
+                <a href="#" class="btn btn-primary">Voir les pilotes</a>
+                <a href="#" class="btn btn-primary">Voir les offres publiée(s)</a>
+                <a href="#" class="btn btn-primary">Voir les entreprises</a>
+            </div>
+        @else
+            @if(Auth::user()->role=="user")
+                <div class="interaction">
+                    <a href="{{ route('profile.offers') }}" class="btn btn-primary">Voir les offres postulées</a>
+                    <a href="#" class="btn btn-primary">voir les stats</a>
+                </div>
+            @endif
+            @if(Auth::user()->role=="pilote")
+                <div class="interaction">
+                    <a href="#" class="btn btn-primary">Voir les offres publiée(s)</a>
+                    <a href="#" class="btn btn-primary">Voir les entreprises</a>
+                </div>
+            @endif
+        @endif
     </div>
 @endsection
