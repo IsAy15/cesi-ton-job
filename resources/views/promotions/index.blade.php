915b96d8a2c26caffe966262f16a48167296941a
SIADOUXValentin <valentin.siadoux@viacesi.fr>
2024-03-20 16:54:26 +0100

    début css promotions

--- resources/views/promotions/index.blade.php
+++ resources/views/promotions/index.blade.php
@@ -1,33 +1,37 @@
 @extends('layouts.home')
 @section('title', 'Liste des promotions')
 @section('content')
-    <h1>Liste des promotions</h1>
-    <a href="{{ route('promotions.create') }}">Ajouter une promotion</a>
-    <table>
-        <thead>
-            <tr>
-                <th>Titre</th>
-                <th>Action</th>
-                <th>Utilisateurs</th> <!-- Nouvelle colonne pour les utilisateurs -->
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($promotions as $promotion)
+@vite('resources/css/promotions.css')
+@vite('resources/css/brouillon-generale.css')
+    <div class="c-1 bg-1">
+        <h1>Liste des promotions</h1>
+        <a href="{{ route('promotions.create') }}" class="btn-1">Ajouter une promotion</a>
+        <table class="">
+            <thead>
                 <tr>
-                    <td>{{ $promotion->name }}</td>
-                    <td>
-                        <a href="{{ route('promotions.edit', $promotion->id) }}">Modifier</a>
-                        <form action="{{ route('promotions.destroy', $promotion->id) }}" method="post">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit">Supprimer</button>
-                        </form>
-                    </td>
-                    <td>
-                        <a href="{{ route('promotions.users', $promotion->id) }}">Voir les utilisateurs</a> <!-- Lien vers la liste des utilisateurs de la promotion -->
-                    </td>
+                    <th>Titre</th>
+                    <th>Action</th>
+                    <th>Utilisateurs</th>
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                @foreach ($promotions as $promotion)
+                    <tr>
+                        <td>{{ $promotion->name }}</td>
+                        <td>
+                            <a href="{{ route('promotions.edit', $promotion->id) }}">Modifier</a>
+                            <form action="{{ route('promotions.destroy', $promotion->id) }}" method="post">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit">Supprimer</button>
+                            </form>
+                        </td>
+                        <td>
+                            <a href="{{ route('promotions.users', $promotion->id) }}">Voir les utilisateurs</a> <!-- Lien vers la liste des utilisateurs de la promotion -->
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 @endsection
