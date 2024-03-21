595aa1c8d45c94daa31d2024542f9d85e99c21e1
SIADOUXValentin <valentin.siadoux@viacesi.fr>
2024-03-21 17:55:57 +0100

    css promotion create

--- resources/views/promotions/create.blade.php
+++ resources/views/promotions/create.blade.php
@@ -1,13 +1,15 @@
 @extends('layouts.home')
 @section('title', 'Ajouter une promotion')
 @section('content')
-    <h1>Ajouter une promotion</h1>
-    <form action="{{ route('promotions.store') }}" method="post">
-        @csrf
-        <div>
-            <label for="name">Nom</label>
-            <input type="text" name="name" id="name">
-        </div>
-        <button type="submit">Ajouter</button>
-    </form>
+@vite('resources/css/brouillon-generale.css')
+    <div class="c-1 bg-1 fit-center">
+        <h1>Ajouter une promotion</h1>
+        <form action="{{ route('promotions.store') }}" method="post" class="form-v">
+            @csrf
+            <div class=liste-v fit-center">
+                <input type="text" name="name" id="name" placeholder="Nom">
+            </div>
+            <button type="submit" class="btn-1">Ajouter</button>
+        </form>
+    </div>
 @endsection
--- resources/views/promotions/edit.blade.php
+++ resources/views/promotions/edit.blade.php
@@ -3,15 +3,15 @@
 @section('content')
 @vite('resources/css/brouillon-generale.css')
     <div class="c-1 bg-1 fit-center">
-    <h1>Modifier la promotion</h1>
-    <form action="{{ route('promotions.update', $promotion->id) }}" method="post" class="form-v">
-        @csrf
-        @method('PUT')
-        <div class="liste-v fit-center">
-            <label for="name">Nom</label>
-            <input type="text" name="name" id="name" value="{{ $promotion->name }}">
-        </div>
-        <button type="submit" class="btn-1">Modifier</button>
-    </form>
-</div>
+        <h1>Modifier la promotion</h1>
+        <form action="{{ route('promotions.update', $promotion->id) }}" method="post" class="form-v">
+            @csrf
+            @method('PUT')
+            <div class="liste-v fit-center">
+                <label for="name">Nom</label>
+                <input type="text" name="name" id="name" value="{{ $promotion->name }}">
+            </div>
+            <button type="submit" class="btn-1">Modifier</button>
+        </form>
+    </div>
 @endsection
