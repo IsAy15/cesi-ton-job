5261778a7f8c4615c5ba4378544e0c24c1bae206
SIADOUXValentin <valentin.siadoux@viacesi.fr>
2024-03-21 19:45:04 +0100

    css compagnies début

--- resources/views/companies/create.blade.php
+++ resources/views/companies/create.blade.php
@@ -1,25 +1,31 @@
 @extends('layouts.home')
 @section('title', 'Ajouter une entreprise')
 @section('content')
-    <h1>Ajouter une entreprise</h1>
-    <form action="{{ route('companies.store') }}" method="post">
-        @csrf
-        <div>
-            <label for="cp_name">Nom de l'entreprise</label>
-            <input type="text" name="cp_name" id="cp_name">
-        </div>
-        <div>
-            <label for="cp_sector">Secteur</label>
-            <input type="text" name="cp_sector" id="cp_sector">
-        </div>
-        <div>
-            <label for="cp_localization">Localisation</label>
-            <input type="text" name="cp_localization" id="cp_localization">
-        </div>
-        <div>
-            <input type="checkbox" name="create_offer" id="create_offer">
-            <label for="create_offer">Créer une offre après l'ajout de l'entreprise</label>
-        </div>
-        <button type="submit">Ajouter</button>
-    </form>
+@vite('resources/css/companies.css')
+@vite('resources/css/brouillon-generale.css')
+@vite('resources/css/checkmark.css')
+    <div class="c-1 bg-1">
+        <h1>Ajouter une entreprise</h1>
+        <form action="{{ route('companies.store') }}" method="post" class="form-v">
+            @csrf
+            <div>
+                <input type="text" name="cp_name" id="cp_name" placeholder="Nom de l'entreprise">
+            </div>
+            <div>
+                <input type="text" name="cp_sector" id="cp_sector" placeholder="Secteur">
+            </div>
+            <div>
+                <input type="text" name="cp_localization" id="cp_localization" placeholder="Localisation">
+            </div>
+            <div class="form-v">
+                <label class="yolo">
+                    <input type="checkbox" name="create_offer" id="create_offer" checked="checked">
+                    <span class="checkmark"></span>
+                </label>
+
+
+                <label for="create_offer">Créer une offre après l'ajout de l'entreprise</label>
+            </div>
+            <button type="submit">Ajouter</button>
+        </form>
 @endsection
--- resources/views/companies/index.blade.php
+++ resources/views/companies/index.blade.php
@@ -1,45 +1,59 @@
 @extends('layouts.home')
 @section('title', 'Liste des entreprises')
 @section('content')
-    <h1>Liste des Entreprises</h1>
-    <input type="text" id="searchInput" placeholder="Rechercher une entreprise...">
-    <a href="{{ route('companies.create') }}" class="btn btn-primary"><i class="fa-solid fa-plus"></i></a>
-
-    @foreach ($companies as $company)
-        <div class="company">
-            <div class="infos">
-                <h2><a href="{{ route('companies.stats', $company->id) }}">{{ $company->name }}</a></h2>
-                <p>Secteur : {{ $company->sector }}</p>
-                <p>Localisation : {{ $company->localization }}</p>
-                <p>Moyenne des notes : {{ $company->average_grade }}</p>
+@vite('resources/css/companies.css')
+@vite('resources/css/brouillon-generale.css')
+    <div class="c-1 bg-1 fit-center">
+        <h1>Liste des Entreprises</h1>
+        <input type="text" id="searchInput" placeholder="Rechercher une entreprise...">
+        <a href="{{ route('companies.create') }}" class="btn-1 btn-2"><i class="fa-solid fa-plus"></i></a>
+        @foreach ($companies as $company)
+            <div class="c-1 bg-2">
+                <div class="liste-v">
+                    <h2><a href="{{ route('companies.stats', $company->id) }}" class="clickable">{{ $company->name }}</a></h2>
+                    <div class="liste-v">
+                        <div class="space">
+                            <p>Secteur :</p>
+                            <p class="elements">{{ $company->sector }}</p>
+                        </div>
+                        <div class="space">
+                            <p>Localisation :</p>
+                            <p class="elements">{{ $company->localization }}</p>
+                        </div>
+                        <div class="space">
+                            <p>Moyenne des notes :</p>
+                            <p class="elements">{{ $company->average_grade }}</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="liste-h">
+                    <a href="{{ route('companies.edit', $company->id) }}" class="btn-1 btn-2"><i class="fa-solid fa-pen-to-square"></i></a>
+                    <form action="{{ route('companies.destroy', $company->id) }}" method="post">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn-1 btn-2"><i class="fa-solid fa-trash"></i></button>
+                    </form>
+                </div>
             </div>
-            <div class="actions">
-                <a href="{{ route('companies.edit', $company->id) }}" class="btn btn-warning"><i class="fa-solid fa-pen-to-square"></i></a>
-                <form action="{{ route('companies.destroy', $company->id) }}" method="post">
-                    @csrf
-                    @method('DELETE')
-                    <button type="submit" class="btn btn-danger"><i class="fa-solid fa-trash"></i></button>
-                </form>
-            </div>
-        </div>
-    @endforeach
-    <script>
-        function filterCompanies() {
-            var input, filter, companies, company, i, txtValue;
-            input = document.getElementById("searchInput");
-            filter = input.value.toUpperCase();
-            companies = document.getElementsByClassName("company");
-            for (i = 0; i < companies.length; i++) {
-                company = companies[i].getElementsByClassName("infos")[0];
-                txtValue = company.textContent || company.innerText;
-                if (txtValue.toUpperCase().indexOf(filter) > -1) {
-                    companies[i].style.display = "";
-                } else {
-                    companies[i].style.display = "none";
+        @endforeach
+        <script>
+            function filterCompanies() {
+                var input, filter, companies, company, i, txtValue;
+                input = document.getElementById("searchInput");
+                filter = input.value.toUpperCase();
+                companies = document.getElementsByClassName("company");
+                for (i = 0; i < companies.length; i++) {
+                    company = companies[i].getElementsByClassName("infos")[0];
+                    txtValue = company.textContent || company.innerText;
+                    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                        companies[i].style.display = "";
+                    } else {
+                        companies[i].style.display = "none";
+                    }
                 }
             }
-        }
 
-        document.getElementById("searchInput").addEventListener("keyup", filterCompanies);
-    </script>
+            document.getElementById("searchInput").addEventListener("keyup", filterCompanies);
+        </script>
+    </div>
 @endsection
