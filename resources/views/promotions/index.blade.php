97b4a1e0e638545e30069e9cf0cbfe17e4514daf
SIADOUXValentin <valentin.siadoux@viacesi.fr>
2024-03-21 12:00:48 +0100

    promotion

--- resources/views/promotions/index.blade.php
+++ resources/views/promotions/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 @vite('resources/css/promotions.css')
 @vite('resources/css/brouillon-generale.css')
-    <div class="c-1 bg-1">
+    <div class="c-1 bg-1 fit-center">
         <h1>Liste des promotions</h1>
         <a href="{{ route('promotions.create') }}" class="btn-1">Ajouter une promotion</a>
         <table class="">
@@ -11,7 +11,6 @@
                 <tr>
                     <th>Titre</th>
                     <th>Action</th>
-                    <th>Utilisateurs</th>
                 </tr>
             </thead>
             <tbody>
@@ -19,15 +18,15 @@
                     <tr>
                         <td>{{ $promotion->name }}</td>
                         <td>
-                            <a href="{{ route('promotions.edit', $promotion->id) }}">Modifier</a>
-                            <form action="{{ route('promotions.destroy', $promotion->id) }}" method="post">
-                                @csrf
-                                @method('DELETE')
-                                <button type="submit">Supprimer</button>
-                            </form>
-                        </td>
-                        <td>
-                            <a href="{{ route('promotions.users', $promotion->id) }}">Voir les utilisateurs</a> <!-- Lien vers la liste des utilisateurs de la promotion -->
+                            <div class="a-001">
+                                <a href="{{ route('promotions.users', $promotion->id) }}" class="btn-1 btn-2"><i class="fa-solid fa-eye"></i></a>
+                                <a href="{{ route('promotions.edit', $promotion->id) }}" class="btn-1 btn-2"><i class="fa-solid fa-pen-to-square"></i></a>
+                                <form action="{{ route('promotions.destroy', $promotion->id) }}" method="post"  class="form-v">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn-1 btn-2"><i class="fa-solid fa-trash"></i></button>
+                                </form>
+                            </div>
                         </td>
                     </tr>
                 @endforeach
