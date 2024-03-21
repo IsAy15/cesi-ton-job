fce8c6fe97cc9114fe79bcd829c6ca072a36d5e5
SIADOUXValentin <valentin.siadoux@viacesi.fr>
2024-03-21 17:52:07 +0100

    css promotion edit

--- resources/views/promotions/edit.blade.php
+++ resources/views/promotions/edit.blade.php
@@ -1,14 +1,17 @@
 @extends('layouts.home')
 @section('title', 'Modifier la promotion')
 @section('content')
+@vite('resources/css/brouillon-generale.css')
+    <div class="c-1 bg-1 fit-center">
     <h1>Modifier la promotion</h1>
-    <form action="{{ route('promotions.update', $promotion->id) }}" method="post">
+    <form action="{{ route('promotions.update', $promotion->id) }}" method="post" class="form-v">
         @csrf
         @method('PUT')
-        <div>
+        <div class="liste-v fit-center">
             <label for="name">Nom</label>
             <input type="text" name="name" id="name" value="{{ $promotion->name }}">
         </div>
-        <button type="submit">Modifier</button>
+        <button type="submit" class="btn-1">Modifier</button>
     </form>
+</div>
 @endsection
