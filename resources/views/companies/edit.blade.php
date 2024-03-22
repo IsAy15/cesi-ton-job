ae15fec853bae1a4b36d85963d82fa9477bdc33d
SIADOUXValentin <valentin.siadoux@viacesi.fr>
2024-03-22 08:41:36 +0100

    css entreprises

--- resources/views/companies/edit.blade.php
+++ resources/views/companies/edit.blade.php
@@ -8,12 +8,18 @@
             @csrf
             @method('PUT')
             <div class="form-v">
+                <div class="input-required fit-center">
                     <label for="cp_name">Nom de l'entreprise</label>
                     <input type="text" name="cp_name" id="cp_name" value="{{ $company->name }}">
+                </div>
+                <div class="input-required fit-center">
                     <label for="cp_sector">Secteur</label>
                     <input type="text" name="cp_sector" id="cp_sector" value="{{ $company->sector }}">
+                </div>
+                <div class="input-required fit-center">
                     <label for="cp_localization">Localisation</label>
                     <input type="text" name="cp_localization" id="cp_localization" value="{{ $company->localization }}">
+                </div>
                 <button type="submit" class="btn-1">Modifier</button>
             </div>
         </form>
