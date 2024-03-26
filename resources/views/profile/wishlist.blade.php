4f3a262b5bf49b5984c395be337fb77540d452aa
SIADOUXValentin <valentin.siadoux@viacesi.fr>
2024-03-26 13:56:41 +0100

    css wishlist

--- resources/views/profile/wishlist.blade.php
+++ resources/views/profile/wishlist.blade.php
@@ -4,26 +4,24 @@
 @vite('resources/css/brouillon-generale.css')
     <div class="c-1 bg-1 fit-center">
         <h2>Vos offres</h2>
-        <div class="liste-v">
-            @if ($wishlist->count() === 0)
-                <p>Votre wishlist est vide.</p>
-            @else
-                <ul>
-                    @foreach($wishlist as $offer)
-                        <li>
+        @if ($wishlist->count() === 0)
+            <p>Votre wishlist est vide.</p>
+        @else
+            <div class="form-v">
+                @foreach($wishlist as $offer)
+                    <div class="c-1 bg-2 space">
                             <a href="{{ route('offers.show', $offer->id) }}">{{ $offer->title }}</a>
                             @auth
                             <form action="{{ route('wishlist.remove', $offer->id) }}" method="post">
                             @csrf
                             @method('DELETE')
-                            <button type="submit">Supprimer de la wishlist</button>
+                            <button type="submit" class="btn-1 btn-2"><i class="fa-solid fa-heart"></i></button>
                         </form>
 
                             @endauth
-                        </li>
-                    @endforeach
-                </ul>
-            @endif
-        </div>
+                    </div>
+                @endforeach
+            </div>
+        @endif
     </div>
 @endsection
