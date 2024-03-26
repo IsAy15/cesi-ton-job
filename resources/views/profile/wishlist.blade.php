fae601004e7bdbb45cc6e456b4153b2f44be7c0d
SIADOUXValentin <valentin.siadoux@viacesi.fr>
2024-03-26 13:45:02 +0100

    css

--- resources/views/profile/wishlist.blade.php
+++ resources/views/profile/wishlist.blade.php
@@ -1,36 +1,29 @@
 @extends('layouts.home')
 @section('title', 'Liste des utilisateurs')
 @section('content')
-<div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Vos offres</div>
-                    <div class="card-body">
-                        @if ($wishlist->count() === 0)
-                            <p>Votre wishlist est vide.</p>
-                        @else
-                            <ul>
-                                @foreach($wishlist as $offer)
-                                    <li>
-                                        <a href="{{ route('offers.show', $offer->id) }}">{{ $offer->title }}</a>
-                                        @auth
-                                        <form action="{{ route('wishlist.remove', $offer->id) }}" method="post">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="submit">Supprimer de la wishlist</button>
-                                    </form>
+@vite('resources/css/brouillon-generale.css')
+    <div class="c-1 bg-1 fit-center">
+        <h2>Vos offres</h2>
+        <div class="liste-v">
+            @if ($wishlist->count() === 0)
+                <p>Votre wishlist est vide.</p>
+            @else
+                <ul>
+                    @foreach($wishlist as $offer)
+                        <li>
+                            <a href="{{ route('offers.show', $offer->id) }}">{{ $offer->title }}</a>
+                            @auth
+                            <form action="{{ route('wishlist.remove', $offer->id) }}" method="post">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit">Supprimer de la wishlist</button>
+                        </form>
 
-                                        @endauth
-                                    </li>
-                                @endforeach
-                            </ul>
-                        @endif
-                    </div>
-                </div>
-            </div>
+                            @endauth
+                        </li>
+                    @endforeach
+                </ul>
+            @endif
         </div>
     </div>
-</body>
-</html>
 @endsection
