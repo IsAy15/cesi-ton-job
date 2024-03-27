189d3762f205fed98c4236163e7233245d69b43a
SIADOUXValentin <valentin.siadoux@viacesi.fr>
2024-03-27 14:53:49 +0100

    d

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -3,6 +3,6 @@
 @section('content')
     <div class="container-1 default-bg fit-center">
         <h1>Contact</h1>
-        <p class="paragraphe">Vous pouvez nous contacter par e-mail à l'adresse <a href="mailto:contact@ctj.fr">contact@ctj.fr</a>, ou par téléphone au <a href="tel:+33782735857">0782735857</a>.</p>
+        <p class="paragraphe">Vous pouvez nous contacter par e-mail à l'adresse <a href="mailto:contact@ctj.fr">contact@ctj.fr</a>, ou par téléphone au <a href="tel:+33782735856">0782735857</a>.</p>
     </div>
 @endsection
