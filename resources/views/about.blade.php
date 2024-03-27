ab9432741a29cabada7bc0670731d9fbdda91ec3
SIADOUXValentin <valentin.siadoux@viacesi.fr>
2024-03-27 14:50:48 +0100

    cs

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,6 +3,41 @@
 @section('content')
     <div class="container-1 default-bg fit-center">
         <h1>A propos</h1>
-        <p>Notre site vous propose des articles sur les dernières tendances en matière de technologie, de culture, de sport, et bien d'autres sujets encore. N'hésitez pas à nous contacter pour nous faire part de vos suggestions, ou pour nous signaler des erreurs.</p>
+        <h2>Bienvenue sur notre plateforme dédiée aux opportunités professionnelles pour les étudiants de CESI !</h2>
+        <h2>À propos de nous :</h2>
+        <p class="paragraphe">
+            Nous sommes une équipe passionnée au service de votre avenir professionnel. Notre mission est de 
+            connecter les étudiants talentueux de CESI avec des opportunités de stage, d'alternance et d'emploi dans 
+            des entreprises de renom. Nous croyons fermement que chaque étudiant mérite la chance de mettre en 
+            pratique ses connaissances et ses compétences dans un environnement professionnel stimulant et enrichissant.
+        </p>
+        <h2>Notre plateforme :</h2>
+        <p class="paragraphe">
+            Notre plateforme offre une interface conviviale et intuitive, conçue pour simplifier votre recherche 
+            d'opportunités. Que vous soyez à la recherche d'un stage pour acquérir une expérience pratique, d'un 
+            contrat en alternance pour combiner études et expérience professionnelle, ou d'un emploi pour débuter 
+            votre carrière, vous trouverez ici une multitude d'offres correspondant à vos aspirations et à votre profil.
+        </p>
+        <h2>Nos partenaires :</h2>
+        <p class="paragraphe">
+            Nous travaillons en étroite collaboration avec un réseau étendu d'entreprises partenaires, soigneusement 
+            sélectionnées pour leur engagement envers le développement des jeunes talents. Ces entreprises offrent des 
+            opportunités dans divers secteurs d'activité, offrant ainsi aux étudiants la possibilité d'explorer 
+            différentes voies professionnelles et de trouver celle qui correspond le mieux à leurs intérêts et à leurs 
+            objectifs de carrière.
+        </p>
+        <h2>Notre engagement :</h2>
+        <p class="paragraphe">
+            En plus de notre plateforme de recherche d'opportunités, nous proposons également des ressources et des 
+            conseils pour vous accompagner tout au long de votre parcours professionnel. Que ce soit pour rédiger un 
+            CV percutant, préparer une entrevue d'embauche ou développer vos compétences professionnelles, notre équipe 
+            est là pour vous soutenir et vous guider vers le succès.
+        </p>
+        <h2>Rejoignez-nous :</h2>
+        <p class="paragraphe">
+            Chez CESI, nous croyons en votre potentiel et nous nous engageons à vous fournir les outils et 
+            les opportunités nécessaires pour réaliser vos ambitions professionnelles. Rejoignez-nous dès aujourd'hui 
+            et commencez à façonner votre avenir avec confiance et détermination.
+        </p>
     </div>
 @endsection
