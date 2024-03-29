297a729ea542c97981fcf3964837ea61c5da4c06
Achraf SOBHI <achrafsobhi.123@gmail.com>
2024-03-29 09:40:25 +0100

    Modification du contact

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -3,6 +3,29 @@
 @section('content')
     <div class="container-1 default-bg fit-center">
         <h1>Contact</h1>
-        <p class="paragraphe">Vous pouvez nous contacter par e-mail à l'adresse <a href="mailto:contact@ctj.fr">contact@ctj.fr</a>, ou par téléphone au <a href="tel:+33782735856">0782735857</a>.</p>
+        <div class="container">
+            <form action="https://api.web3forms.com/submit" method="POST">
+                <input type="hidden" name="access_key" value="bff74283-e893-450f-ba71-6c451066d92a">
+                <div>
+                    <input type="text" name="name" placeholder="Prénom NOM" required>
+                    <input type="email" name="email" placeholder="Email" required>
+                </div>
+                <div>
+                    <input type="text" name="phone" placeholder="Numero de téléphone">
+                    <input type="text" name="subject" placeholder="Objet du message">
+                </div>
+                <textarea name="message" cols="30" rows="10" placeholder="Votre message" required></textarea>
+                <div class="captcha-container">
+                    <div class="h-captcha" data-captcha="true"></div>
+                </div>
+                <input type="hidden" name="redirect" value="https://web3forms.com/success">
+                <input type="submit" value="Envoyez mon message" class="btn">
+                <script src="https://web3forms.com/client/script.js" async defer></script>
+            </form>
+        </div>
     </div>
+    <div class="container-1 default-bg fit-center">
+        <h2>Informations de contact</h2>
+        <p class="paragraphe">Vous pouvez également nous contacter par e-mail à l'adresse <a href="mailto:contact@ctj.fr">contact@ctj.fr</a>, ou par téléphone au <a href="tel:+33782735856">0782735857</a>.</p>
+        </div>
 @endsection
