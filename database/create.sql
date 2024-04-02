a92e2d2ace139b040c9382e0f6ccaa01b6afdd11
IsAy15 <40857644+IsAy15@users.noreply.github.com>
2024-04-02 14:16:26 +0200

    Modif bdd

--- database/create.sql
+++ database/create.sql
@@ -176,15 +176,16 @@ INSERT INTO `users` (`id`, `lastname`, `firstname`, `email`, `password`, `role`,
 -- Remplissage de la table companies
 DELETE FROM companies;
 
-INSERT INTO companies (name, sector, localization) VALUES
-('Dassault Systèmes', 'Informatique', '{"code": "78640", "cp": "78140", "dep": "78"}'),
-('Capgemini', 'Informatique', '{"code": "75056", "cp": "75001", "dep": "75"}'),
-('Thales', 'Informatique', '{"code": "92026", "cp": "92400", "dep": "92"}'),
-('Airbus', 'Aéronautique', '{"code": "31000", "cp": "31670", "dep": "31"}'),
-('Atos', 'Informatique', '{"code": "95063", "cp": "95870", "dep": "95"}'),
-('Sopra Steria', 'Informatique', '{"code": "75056", "cp": "75001", "dep": "75"}'),
-('Ubisoft', 'Informatique', '{"code": "93048", "cp": "93100", "dep": "93"}'),
-('OVHcloud', 'Informatique', '{"code": "59512", "cp": "59100", "dep": "59"}');
+INSERT INTO `companies` (`id`, `name`, `sector`, `localization`) VALUES
+(1, 'Dassault Systèmes', 'Informatique', '[{\"nom\": \"Vélizy-Villacoublay\", \"code\": \"78640\", \"cp\": \"78140\", \"dep\": \"78\"}]'),
+(2, 'Capgemini', 'Informatique', '[{\"nom\": \"Paris\", \"code\": \"75056\", \"cp\": \"75001\", \"dep\": \"75\"}]'),
+(3, 'Thales', 'Informatique', '[{\"nom\": \"Courbevoie\", \"code\": \"92026\", \"cp\": \"92400\", \"dep\": \"92\"}]'),
+(4, 'Airbus', 'Aéronautique', '[{\"nom\": \"Labège\", \"code\": \"31000\", \"cp\": \"31670\", \"dep\": \"31\"}]'),
+(5, 'Atos', 'Informatique', '[{\"nom\": \"Bezons\", \"code\": \"95063\", \"cp\": \"95870\", \"dep\": \"95\"}]'),
+(6, 'Sopra Steria', 'Informatique', '[{\"nom\": \"Paris\", \"code\": \"75056\", \"cp\": \"75001\", \"dep\": \"75\"}]'),
+(7, 'Ubisoft', 'Informatique', '[{\"nom\": \"Montreuil\", \"code\": \"93048\", \"cp\": \"93100\", \"dep\": \"93\"}]'),
+(8, 'OVHcloud', 'Informatique', '[{\"nom\": \"Roubaix\", \"code\": \"59512\", \"cp\": \"59100\", \"dep\": \"59\"}]'),
+(10, 'Inetum', 'Informatique', '[{\"nom\":\"Labège\",\"code\":\"31254\",\"cp\":\"31670\",\"dep\":\"31\"},{\"nom\":\"Toulouse\",\"code\":\"31555\",\"cp\":\"31000\",\"dep\":\"31\"}]');
 
 -- Remplissage de la table levels
 DELETE FROM levels;
