da467a4b4cb5cfce89ca9210054562433c8cbc64
ReddenAZS <145404197+ReddenAZS@users.noreply.github.com>
2024-04-04 17:40:03 +0200

    Pagination css terminer et variable dans général

--- resources/views/vendor/pagination/tailwind.blade.php
+++ resources/views/vendor/pagination/tailwind.blade.php
@@ -3,18 +3,18 @@
 @if ($paginator->hasPages())
     <nav role= aria-label="{{ __('Pagination Navigation') }}"">
         <div>
-            <div class="pag-font ecart">
+            <div class="pag-font">
                 <span>
                     {{-- Previous Page Link --}}
                     @if ($paginator->onFirstPage())
-                        <span class="no_under" aria-disabled="true" aria-label="{{ __('pagination.previous') }}">
+                        <span class="ecart-r no_under" aria-disabled="true" aria-label="{{ __('pagination.previous') }}">
                             <span aria-hidden="true">
-                            <i class="fa-solid fa-arrow-left"></i>
+                            <i class="pag-el fa-solid fa-arrow-left"></i>
                             </span>
                         </span>
                     @else
-                        <a class="no_under" href="{{ $paginator->previousPageUrl() }}" rel="prev" aria-label="{{ __('pagination.previous') }}">
-                            <i class="fa-solid fa-arrow-left"></i>
+                        <a class="ecart-r no_under" href="{{ $paginator->previousPageUrl() }}" rel="prev" aria-label="{{ __('pagination.previous') }}">
+                            <i class="pag-el fa-solid fa-arrow-left"></i>
                         </a>
                     @endif
 
@@ -22,7 +22,7 @@
                     @foreach ($elements as $element)
                         {{-- "Three Dots" Separator --}}
                         @if (is_string($element))
-                            <span class="no_under" aria-disabled="true">
+                            <span class="pag-el ecart-l no_under" aria-disabled="true">
                                 <span>{{ $element }}</span>
                             </span>
                         @endif
@@ -31,11 +31,11 @@
                         @if (is_array($element))
                             @foreach ($element as $page => $url)
                                 @if ($page == $paginator->currentPage())
-                                    <span  class="no_under" aria-current="page">
-                                        <span>{{ $page }}</span>
+                                    <span  class="ecart-b pag-el" aria-current="page">
+                                        <span class="select">{{ $page }}</span>
                                     </span>
                                 @else
-                                    <a class="no_under" href="{{ $url }}" >
+                                    <a class="ecart-b pag-el no_under" href="{{ $url }}" >
                                         {{ $page }}
                                     </a>
                                 @endif
@@ -45,13 +45,13 @@
 
                     {{-- Next Page Link --}}
                     @if ($paginator->hasMorePages())
-                        <a class="no_under" href="{{ $paginator->nextPageUrl() }}" rel="next" aria-label="{{ __('pagination.next') }}">
+                        <a class="pag-el ecart-l no_under" href="{{ $paginator->nextPageUrl() }}" rel="next" aria-label="{{ __('pagination.next') }}">
                         <i class="fa-solid fa-arrow-right"></i>
                         </a>
                     @else
-                        <span class="no_under" aria-disabled="true" aria-label="{{ __('pagination.next') }}">
-                            <span class="no_under" aria-hidden="true">
-                            <i class="fa-solid fa-arrow-right"></i>
+                        <span class="pag-el no_under" aria-disabled="true" aria-label="{{ __('pagination.next') }}">
+                            <span class="pag-el ecart-l no_under" aria-hidden="true">
+                            <i class="pag-el fa-solid fa-arrow-right"></i>
                             </span>
                         </span>
                     @endif
